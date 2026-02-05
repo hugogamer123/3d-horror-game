67078646d435ae62f0d76d3d19cf67bdc24a59cb
hugogamer123 <81802978+hugogamer123@users.noreply.github.com>
2026-02-05 23:52:49 +0100

    did lots of stuff
    Flashlight works, resolution is nerfed, movement works, shit looking good

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,11 +7,23 @@ public class Movement : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float JumpForce;
     [SerializeField] private float Speed;
+    
+    [SerializeField] private float MouseSensitivityX = 1;
+    [SerializeField] private float MouseSensitivityY = 1;
+
+    Vector2 look;
+    [SerializeField] private Transform camTransform;
+    [SerializeField] private Transform playerTransform;
 
     //Old input system stuff
     private float horizontal;
     private float vertical;
     Vector3 moveDirections;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void Update()
     {
@@ -20,6 +32,13 @@ public class Movement : MonoBehaviour
         {
             rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
         }
+
+        look.x += UnityEngine.Input.GetAxis("Mouse X") * MouseSensitivityX;
+        look.y += UnityEngine.Input.GetAxis("Mouse Y") * MouseSensitivityY;
+
+        look.y = Mathf.Clamp(look.y, -89f, 89f);
+        playerTransform.localRotation = Quaternion.Euler(0, look.x, 0);
+        camTransform.localRotation = Quaternion.Euler(-look.y, 0, 0);
     }
 
     void FixedUpdate()
@@ -28,10 +47,21 @@ public class Movement : MonoBehaviour
         float x = UnityEngine.Input.GetAxis("Horizontal");
         float z = UnityEngine.Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(x, 0f, z) * Speed;
+        /*Vector3 move = new Vector3(x, 0f, z) * Speed;
         Vector3 velocity = rb.linearVelocity;
         velocity.x = move.x;
         velocity.z = move.z;
-        rb.linearVelocity = velocity;
+
+        Vector3 worldMove = transform.TransformVector(move.x, 0, move.z);
+
+        rb.linearVelocity = velocity worldMove * Speed; */
+
+        Vector3 move =
+            playerTransform.forward * z +
+            playerTransform.right * x;
+        move *= Speed;
+
+        Vector3 velocity = rb.linearVelocity;
+        rb.linearVelocity = new Vector3(move.x, velocity.y, move.z);
     }
 }
