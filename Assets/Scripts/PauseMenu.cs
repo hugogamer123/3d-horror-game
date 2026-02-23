ffe6d9ca8215c981a2a7cd3fa2310f6fe62fab6d
hugogamer123 <hugo123mcnally@gmail.com>
2026-02-23 13:07:27 +0100

    added things

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,14 +1,24 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject MenuUI;
+    [SerializeField] private Slider mouseXslider;
+    [SerializeField] private Slider mouseYslider;
     bool IsPaused;
 
     void Awake()
     {
         MenuUI.SetActive(false);
+        mouseXslider.minValue = 0f;
+        mouseXslider.maxValue = 10f;
+        mouseYslider.minValue = 0f;
+        mouseYslider.maxValue = 10f;
+
+        mouseXslider.value = 3f;
+        mouseYslider.value = 3f;
     }
     void Update()
     {
